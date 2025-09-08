5f881b092fe22f932608aca9ca529397c3aa5ee0
amicidimaria <egozen.design@gmail.com>
2025-09-08 17:31:21 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// --- CONFIGURAZIONE ---
+// In questo file sono salvate le informazioni sensibili.
+// NOTA: Questo file è comunque visibile pubblicamente.
+
+const AIRTABLE_CONFIG = {
+    baseId: 'appsDydZ1WjNQ6sZI',
+    tableName: 'svincolati', // Aggiornato da 'mercato'
+    tradesTableName: 'scambi', // Aggiunta la nuova tabella per gli scambi
+    // ATTENZIONE: Sostituisci la chiave qui sotto con il tuo nuovo token di Airtable
+    readApiKey: 'patGRp7zzRpPQeM1w.76c6bbb8b4e24312f85e38a30d10acc9f8112d7445a8032930539338d021f0d0', // Chiave di SOLA LETTURA
+    writeApiKey: 'patGRp7zzRpPQeM1w.76c6bbb8b4e24312f85e38a30d10acc9f8112d7445a8032930539338d021f0d0'  // Chiave di SCRITTURA
+};
+
+const TEAM_PASSWORDS = {
+    "Gonzi": "7545", "Chivas": "0184", "Minturno": "6528", "Califfo": "2187", "BDS": "2575",
+    "Diavolone": "1191", "RealSpillo": "5764", "Pandamonio": "9492", "SpaccioUno": "0604", "SantaMasterella": "7532"
+};
